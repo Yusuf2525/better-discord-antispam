4fd71bade80a8b829d28a323203ab6056aeeb30e
Yusuf2525 <46084253+Yusuf2525@users.noreply.github.com>
2019-10-05 04:27:52 +0300

    Update better_antispam.js

--- better_antispam.js
+++ better_antispam.js
@@ -11,15 +11,15 @@ module.exports = async (client, options) => {
   const limitUntilWarn = (options && options.limitUntilWarn) || 3; // Default value: 3. Explication: This is the limit where you get the warn message. If the member X sent over 3 messages within the interval, he get warned
   const limitUntilMuted = (options && options.limitUntilMuted) || 5; // Default value: 5. Explication: This is the limit where you get Punished. If the member X sent over 5 messages within the interval, he get muted.
   const interval = (options && options.interval) || 2000; //Default Time: 2000MS (1000 milliseconds = 1 second, 2000 milliseconds = 2 seconds etc...). Explication: The interval where the messages are sent. Practically if member X sent 5+ messages within 2 seconds, he get muted
-  const warningMessage = (options && options.warningMessage) || "if you don't stop from spamming, I'm going to punish you!"; // Default Message: if you don't stop from spamming, I'm going to punish you!. Explication: None, it's just a message you get for the warning phase.
-  const muteMessage = (options && options.muteMessage) || "was muted since we don't like too much advertisement type people!"; // Default Message: "was muted since we don't like too much advertisement type people!". Explication: The message sent after member X was punished
+  const warningMessage = (options && options.warningMessage) || "Spam yapma yoksa sustururum!"; // Default Message: if you don't stop from spamming, I'm going to punish you!. Explication: None, it's just a message you get for the warning phase.
+  const muteMessage = (options && options.muteMessage) || "Spam yaptığın için susturuldun!"; // Default Message: "was muted since we don't like too much advertisement type people!". Explication: The message sent after member X was punished
   const maxDuplicatesWarning = (options && options.maxDuplicatesWarning || 7); // Default value: 7. Explication: When people are spamming the same message, <limitUntilWarn> is ignored and this will trigger when member X sent over 7+ message that are the same.
   const maxDuplicatesMute = (options && options. maxDuplicatesMute || 10); // Deafult value: 10 Explication: The limit where member X get muted after sending too many messages(10+).
   const ignoredRoles = (options && options.ignoredRoles) || []; // Default value: None. Explication: The members with this role(or roles) will be ignored if they have it. Suggest to not add this to any random guys.
   const ignoredMembers = (options && options.ignoredMembers) || []; // Default value: None. Explication: These members are directly affected and they do not require to have the role above. Good for undercover pranks.
-  const mutedRole = (options && options.mutedRole) || "muted"; // Default value: muted. Explication: Here you put the name of the role that should not let people write/speak or anything else in your server. If there is no role set, by default, the module will attempt to create the role for you & set it correctly for every channel in your server. It will be named "muted".
+  const mutedRole = (options && options.mutedRole) || "Susturuldu"; // Default value: muted. Explication: Here you put the name of the role that should not let people write/speak or anything else in your server. If there is no role set, by default, the module will attempt to create the role for you & set it correctly for every channel in your server. It will be named "muted".
   const timeMuted = (options && options.timeMuted) || 1000 * 600; // Default value: 10 minutes. Explication: This is how much time member X will be muted. if not set, default would be 10 min.
-  const logChannel = (options && options.logChannel) || "AntiSpam-logs"; // Default value: "AhtiSpam-logs". Explication: This is the channel where every report about spamming goes to. If it's not set up, it will attempt to create the channel.
+  const logChannel = (options && options.logChannel) || "spam-log"; // Default value: "AhtiSpam-logs". Explication: This is the channel where every report about spamming goes to. If it's not set up, it will attempt to create the channel.
 
 // If something is added wrong, throw an error
 
@@ -74,7 +74,7 @@ module.exports = async (client, options) => {
       let ReportChannel = m.guild.channels.find(ch => ch.name === logChannel);
       if(!ReportChannel){
         try{
-            ReportChannel = await m.guild.createChannel('AntiSpam-logs', {
+            ReportChannel = await m.guild.createChannel('spam-log', {
               type: 'text',
               permissionOverwrites:[{
                 id: m.guild.id,
@@ -93,7 +93,7 @@ module.exports = async (client, options) => {
       if (!role) {
         try {
             role = await m.guild.createRole({
-                name: "muted",
+                name: "Susturuldu",
                 color: "#000000",
                 permissions: []
             })
@@ -116,25 +116,24 @@ module.exports = async (client, options) => {
         user.addRole(role).then(()=>{
           m.channel.send(`<@!${m.author.id}>, ${muteMsg}`);
           let muteEmbed = new RichEmbed()
-            .setAuthor(' Action | Auto Mute', `https://images-ext-2.discordapp.net/external/Wms63jAyNOxNHtfUpS1EpRAQer2UT0nOsFaWlnDdR3M/https/image.flaticon.com/icons/png/128/148/148757.png`)
-            .addField('Member muted:',`${user}`)
-            .addField(`How much time got muted?:`,`${timeMuted} seconds (10 min)`)
-            .addField('Reason of mute: ', `Spam`)
-            .addField(`When it was muted that person:`,TheDate+ " at "+ clock+" "+amORpm)
+            .setAuthor(' Aeseria Satış Platformu | Susturma', `https://images-ext-2.discordapp.net/external/Wms63jAyNOxNHtfUpS1EpRAQer2UT0nOsFaWlnDdR3M/https/image.flaticon.com/icons/png/128/148/148757.png`)
+            .addField('Susturulan Üye:',`${user}`)
+            .addField(`Susturulma Süresi:`,`10 Dakika`)
+            .addField('Sebep ', `Spam`)
             .setColor('#D9D900')
           ReportChannel.send(muteEmbed);
           setTimeout(()=>{
             user.removeRole(role);
             let unmutedEmbed = new RichEmbed()
-              .setAuthor('Action | Auto Unmute')
-              .addField(`Member unmuted:`,`${user}`)
-              .addField(`Reason of unmute:`,`Time Expired(10 min)`)
+              .setAuthor('Aeseria Satış Platformu | Susturma Açma')
+              .addField(`Susturulan Üye:`,`${user}`)
+              .addField(`Suskun Olduğu Süre:`,`10 Dakika`)
               .setColor('#D9D900')
           ReportChannel.send(unmutedEmbed)
           }, timeMuted);
           return true;
        }).catch((e) => {
-          m.guild.owner.send(`Oops, seems like i don't have sufficient permissions to mute <@!${message.author.id}>!\n It can be that or another type of error happened! Tell me on github: https://github.com/MirageZoe/ \n Everything happened on ${TheDate} at ${clock} ${amORpm} with message:\n\n\`${e.message}\`\n\n *P.S: If this is the first time getting something like this, most likely because it was not set up good the log channel at beginning and didn't know where to send the reports. Do not panic, next time it will work since he created the channel where to send the reports!*`);
+         ReportChannel.send(`Uyarı: <@!${message.author.id}> adlı üye spam yapıyor ve ben engelleyemiyorum. Bi el atın.`);
           return false;
       });
     }//end of user
